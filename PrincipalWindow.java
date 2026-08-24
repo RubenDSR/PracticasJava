f0641d221956c500bdd7c5934a778db42f9a344f
Ruben <rubentrader1984@outlook.com>
2026-08-25 00:58:07 +0100

    Primer commit

---
+++ PrincipalWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class PrincipalWindow extends JFrame
+{
+    public PrincipalWindow()
+    {
+        setTitle("Principal Window");
+        setBounds( 600, 600, 600, 600 );
+        setVisible(true);
+    }
+}
