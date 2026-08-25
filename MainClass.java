5f19eaea4cdf4e4bcd29a210a66e5acf0e0b9fa3
Ruben <rubentrader1984@outlook.com>
2026-08-25 01:02:55 +0100

    Main class is created

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass
+{
+    private static PrincipalWindow principal_window;
+    public static void main(String[] args)
+    {
+        principal_window = new PrincipalWindow();
+    }
+}
